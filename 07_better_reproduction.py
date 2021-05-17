4c6d04219881a453777aa9305b80ba2269c015c3
William Power <willpowe@gmail.com>
2021-05-17 12:34:47 -0400

    removing progressbar.

--- 07_better_reproduction.py
+++ 07_better_reproduction.py
@@ -4,7 +4,6 @@ import torch
 import torch_geometric.utils as U
 import torch.nn.parameter as P
 import torch.nn.functional as F
-import progressbar
 from torch.nn import Linear
 from torch_geometric.nn import GCNConv 
 
